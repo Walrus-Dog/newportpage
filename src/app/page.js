c2ff86410c972b29ebdcf53fd3de2c1a0285056b
Walrus-Dog <walrusdog@gmail.com>
2023-08-02 22:39:06 -0400

    Ver1.021
    Quick vh fixes on vid page

--- src/app/page.js
+++ src/app/page.js
@@ -17,9 +17,6 @@ export default function Home() {
             <div className="row">
                 <video className="bg-vid" playsinline autoPlay loop muted poster="/media/drawlogoloop1.gif" src="/media/landingvideo6.mp4"></video>
                 <div class="vid-text">
-                    <a href="landing.html">
-                        <img className="logo-img-header-landing" src="/media/drawlogoloop1.gif" alt="nonologo" />
-                    </a>
                     <h1 className="lander-text-header">walrusdog</h1>
                     <p className="lander-text-desc">art | game design | web dev</p>
                     <p className="lander-text-desc">let's get started, yeah?</p>
