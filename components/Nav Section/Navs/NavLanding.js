55de1f66503be3af78bba5570d7d0809ba7f7897
Walrus-Dog <walrusdog@gmail.com>
2023-08-10 18:31:36 -0400

    Ver 1.1
    Big screen fix! The site should now function normally on 1440p monitors!

--- components/Nav Section/Navs/NavLanding.js
+++ components/Nav Section/Navs/NavLanding.js
@@ -4,7 +4,7 @@ import Link from "next/link"
 export default function NavLanding() {
     return (
         <section className="nav-section nav-landing">
-            <div className="nav-wrapper">
+            <div className="nav-wrapper d-sm-none d-md-block d-lg-block d-xl-block d-xxl-block">
                 <div className="nav-wrapper-inner">
                     <Link className="landing-link nav-link1" href="/about">| about</Link>
                     <Link className="landing-link nav-link2" href="/art">| art</Link>
