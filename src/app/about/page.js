0fc546c12cf27dba7dbe5bf3847b8006de859a5c
Walrus-Dog <walrusdog@gmail.com>
2024-03-27 15:25:06 -0500

    3.12
    Small tweaks

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -35,7 +35,7 @@ export default function About() {
                                 <p class="about-text">
                                     <p>Hello there!</p>
                                     <p>My name is Patrick, also known as WalrusDog online. I am a web developer dipping my toes into the game dev and digital art scene. Despite my growing skillset, I am a ravenous learner and I tend to think my creative side helps me out even in my technical work.</p>
-                                    <p>I graduated from Mississippi Coding Academies in 2022 and I am very passionate about many different types of projects. I am currently proficient in HTML, CSS, JS, NEXT, React, C#, Unity, Python, MySQL, and I am willing to learn wnew and exciting toolsets to finish many types of projects</p>
+                                    <p>I graduated from Mississippi Coding Academies in 2022 and I am very passionate about many different types of projects. I am currently proficient in HTML, CSS, JS, NEXT, React, C#, Unity, Python, MySQL, and I am willing to learn new and exciting toolsets to finish many types of projects</p>
                                     <p>I created this website to showcase my various interests including examples of my art, various game and web dev projects, and even my journey in becoming a fluent Japanese speaker. I hope you enjoy exploring my adventures as much as I have enjoyed creating them.</p>
                                     <p>If you are interested in contacting me about anything you see on my website, if you are interested in working on a project together, or interested in hiring me, please reach me by email at <span className="about-email"><a href="">walrusdog@gmail.com</a></span>.</p>
                                 </p>
